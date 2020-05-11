bfb491003e4f0f4aa1b2f63576fa4ab09611d0ce
Tomas Kudelka <tomas@appsdevteam.com>
2020-05-11 07:37:39 +0200

    opravy

--- src/FormRenderer.php
+++ src/FormRenderer.php
@@ -6,9 +6,15 @@ use ADT\Forms\Controls\PhoneNumberInput;
 use Nette;
 use Nette\Forms\Rendering\DefaultFormRenderer;
 use Nette\Utils\Html;
+use Nette\Utils\IHtmlString;
 
 class FormRenderer extends DefaultFormRenderer
 {
+	public function __construct(Nette\Forms\Form $form)
+	{
+		$this->form = $form;
+	}
+
 	public function renderLabel(Nette\Forms\IControl $control): Html
 	{
 		if ($control->getLabel() && $control->getLabel()->getHtml()) {
@@ -20,7 +26,43 @@ class FormRenderer extends DefaultFormRenderer
 
 	public function renderControl(Nette\Forms\IControl $control): Html
 	{
-		$el = parent::renderControl($control);
+		$body = $this->getWrapper('control container');
+		if ($this->counter % 2) {
+			$body->class($this->getValue('control .odd'), true);
+		}
+		if (!$this->getWrapper('pair container')->getName()) {
+			$body->class($control->getOption('class'), true);
+			$body->id = $control->getOption('id');
+		}
+
+		$description = $control->getOption('description');
+		if ($description instanceof IHtmlString) {
+			$description = ' ' . $description;
+
+		} elseif ($description != null) { // intentionally ==
+			if ($control instanceof Nette\Forms\Controls\BaseControl) {
+				$description = $control->translate($description);
+			}
+			$description = ' ' . $this->getWrapper('control description')->setText($description);
+
+		} else {
+			$description = '';
+		}
+
+		if ($control->isRequired()) {
+			$description = $this->getValue('control requiredsuffix') . $description;
+		}
+
+		$control->setOption('rendered', true);
+		$el = $control->getControl();
+		if ($el instanceof Html) {
+			if ($el->getName() === 'input') {
+				$el->class($this->getValue("control .$el->type"), true);
+			}
+			$el->class($this->getValue('control .error'), $control->hasErrors());
+		}
+
+		$el = $body->setHtml($el . $description . $this->doRenderErrors($control));
 
 		// Is this an instance of a RadioList or CheckboxList?
 		if (
@@ -61,9 +103,50 @@ class FormRenderer extends DefaultFormRenderer
 			$el = Html::el('div')
 				->setAttribute('class', 'form-row')
 				->addHtml('<div class="col-4">' . $control->getControlPart(PhoneNumberInput::CONTROL_COUNTRY_CODE)->addClass('form-control') . '</div>')
-				->addHtml('<div class="col-8">' . $control->getControlPart(PhoneNumberInput::CONTROL_NATIONAL_NUMBER)->addClass('form-control') . "</div>");
+				->addHtml('<div class="col-8">' . $control->getControlPart(PhoneNumberInput::CONTROL_NATIONAL_NUMBER)->addClass('form-control') . $description . $this->doRenderErrors($control) . '</div>');
 		}
 
 		return $el;
+	}
+
+	/**
+	 * Renders validation errors (per form or per control).
+	 */
+	public function renderErrors(Nette\Forms\IControl $control = null, bool $own = true): string
+	{
+		$errors = $control
+			? $control->getErrors()
+			: ($own ? $this->form->getOwnErrors() : $this->form->getErrors());
+		return $this->doRenderErrors($control, $errors);
+	}
+
+	public function doRenderErrors(Nette\Forms\IControl $control = null, $errors = []): string
+	{
+		$container = $this->getWrapper($control ? 'control errorcontainer' : 'error container');
+		$item = $this->getWrapper($control ? 'control erroritem' : 'error item');
+
+		$elId = ($control ? $control->getHtmlId(): $this->form->getElementPrototype()->getAttribute('id'));
+
+		$container->setAttribute('id', 'snippet-' . $elId . '-errors');
+
+		if ($control) {
+			$errors = $control->getErrors();
+		}
+
+		foreach ($errors as $error) {
+			$item = clone $item;
+			if ($error instanceof IHtmlString) {
+				$item->addHtml($error);
+			} else {
+				$item->setText($error);
+			}
+			$container->addHtml($item);
+		}
+
+		if ($control && $errors) {
+			$container->addHtml('<script>document.getElementById("' . $elId . '").classList.add("is-invalid");</script>');
+		}
+
+		return "\n" . $container->render($control ? 1 : 0);
 	}
 }
