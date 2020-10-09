d0b569071b49736a2934927f56b4771feb0a9236
Tomáš Kudělka <thorewi@gmail.com>
2020-10-09 10:48:58 +0200

    Update FormRenderer.php

--- src/FormRenderer.php
+++ src/FormRenderer.php
@@ -102,8 +102,8 @@ class FormRenderer extends DefaultFormRenderer
 		elseif ($control instanceof PhoneNumberInput) {
 			$el = Html::el('div')
 				->setAttribute('class', 'form-row')
-				->addHtml('<div class="col-4">' . $control->getControlPart(PhoneNumberInput::CONTROL_COUNTRY_CODE)->addClass('form-control') . '</div>')
-				->addHtml('<div class="col-8">' . $control->getControlPart(PhoneNumberInput::CONTROL_NATIONAL_NUMBER)->addClass('form-control') . $description . $this->doRenderErrors($control) . '</div>');
+				->addHtml('<div class="col-5">' . $control->getControlPart(PhoneNumberInput::CONTROL_COUNTRY_CODE)->addClass('form-control') . '</div>')
+				->addHtml('<div class="col-7">' . $control->getControlPart(PhoneNumberInput::CONTROL_NATIONAL_NUMBER)->addClass('form-control') . $description . $this->doRenderErrors($control) . '</div>');
 		}
 
 		return $el;
