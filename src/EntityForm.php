e1e931dc53fdd188318e8dfd54fad95ee9eda846
Tomáš Kudělka <thorewi@gmail.com>
2020-11-17 12:18:17 +0000

    Update EntityForm.php

--- src/EntityForm.php
+++ src/EntityForm.php
@@ -2,6 +2,7 @@
 
 namespace ADT\BaseForm;
 
+use ADT\DoctrineForms\ToOneContainer;
 use ADT\EmailStrictInput;
 use ADT\Forms\Controls\CurrencyInput;
 use ADT\Forms\Controls\PhoneNumberInput;
@@ -9,6 +10,7 @@ use Kdyby\Replicator\Container;
 use Nette\Application\UI\Form;
 use Nette\Forms\IFormRenderer;
 use Vodacek\Forms\Controls\DateInput;
+use Closure;
 
 /**
  * @method Container addDynamic($name, $factory, $createDefault = 0, $forceDefault = FALSE)
@@ -16,6 +18,8 @@ use Vodacek\Forms\Controls\DateInput;
  * @method PhoneNumberInput addPhoneNumber($name, $label = null)
  * @method EmailStrictInput addEmailStrict($name, $label = null, $errorMessage = 'Invalid email address.')
  * @method CurrencyInput addCurrency($name, $label = null, $currency = null)
+ * @method ToOneContainer toOne(string $name, Closure $containerFactory, ?Closure $entityFactory = null, ?string $isFilledComponentName = null, ?string $isRequiredMessage = null)
+ * @method ToManyContainer toMany(string $name, Closure $containerFactory, ?Closure $entityFactory = null, ?string $isFilledComponentName = null, ?string $isRequiredMessage = null)
  */
 class EntityForm extends Form
 {
