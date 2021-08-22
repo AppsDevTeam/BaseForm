fa9f47ba624e54878a39c5d0efb832ee723f6b35
Tomáš Kudělka <thorewi@gmail.com>
2021-08-22 13:38:10 +0200

    Update BaseForm.php

--- src/BaseForm.php
+++ src/BaseForm.php
@@ -1,198 +1,67 @@
 <?php
 
-namespace ADT\BaseForm;
-
-use Nette\Application\UI\Control;
-use Nette\Application\UI\Presenter;
-use Nette\Forms\Form;
-use Nette\Forms\IControl;
+namespace ADT\DoctrineForms;
 
 /**
- * @property-read EntityForm $form
- * @method onBeforeInit($form)
- * @method onAfterInit($form)
+ * @property-read Form $form
  * @method onAfterMapToForm($form)
  * @method onAfterMapToEntity($form)
- * @method onAfterProcess($form)
  */
-abstract class BaseForm extends Control
+abstract class BaseForm extends \ADT\Forms\BaseForm
 {
-	/** @var string|null */
-	public ?string $templateFilename = null;
+	protected $entity;
 
-	/** @var bool */
-	public bool $isAjax = true;
-
-	/** @var bool */
-	public bool $emptyHiddenToggleControls = false;
-
-	/** @var callable[] */
-	public $onBeforeInit = [];
-
-	/** @var callable[] */
-	public $onAfterInit = [];
-
-	/** @var callable[] */
+	/**
+	 * @internal
+	 * @var callable[]
+	 */
 	public $onAfterMapToForm = [];
 
-	/** @var callable[] */
+	/**
+	 * @internal
+	 * @var callable[]
+	 */
 	public $onAfterMapToEntity = [];
-
-	/** @var callable[] */
-	public $onAfterProcess = [];
-
-	protected $row;
 
 	public function __construct()
 	{
-		$this->monitor(Presenter::class, function($presenter) {
-			$form = $this->getForm();
+		parent::__construct();
 
-			/** @link BaseForm::validateFormCallback() */
-			/** @link BaseForm::processFormCallback() */
-			foreach(['onValidate' => 'validateFormCallback', 'onSuccess' => 'processFormCallback'] as $event => $callback) {
-				// first argument of array_unshift has to be an array
-				if ($form->$event === null) {
-					$form->$event = [];
-				}
-
-				// we want default events to be executed first
-				array_unshift($form->$event, [$this, $callback]);
-			}
-
-			$this->onBeforeInit($form);
-
-			if ($this->row) {
-				$form->setEntity($this->row);
-			}
-
-			$this->init($form);
-
-			$this->onAfterInit($form);
-
-			if ($this->row) {
-				$form->mapToForm();
-
-				$this->onAfterMapToForm($form);
-			}
-
-			if ($form->isSubmitted()) {
-				if (is_bool($form->isSubmitted())) {
-					$form->setSubmittedBy(null);
-				}
-				elseif ($form->isSubmitted()->getValidationScope() !== null) {
-					$form->onValidate = [];
-				}
+		$this->setOnBeforeInitForm(function($form) {
+			if ($this->entity) {
+				$form->setEntity($this->entity);
 			}
 		});
-	}
 
-	/**
-	 * @param Form $form
-	 */
-	public function validateFormCallback($form)
-	{
-		if (!method_exists($this, 'validateForm')) {
-			return;
-		}
+		// we don't call setter intentionally to avoid logic in setter
+		$this->onAfterInitForm[] = [$this, 'initOnAfterMapToForm'];
 
-		if ($this->row) {
-			$this->validateForm($form->getEntity());
-		}
-		else {
-			$this->validateForm($form->getUnsafeValues(null));
-		}
-	}
+		$this->setOnBeforeProcessForm(function($form) {
+			if ($this->getForm()->getEntity()) {
+				$this->getForm()->mapToEntity();
 
-	private function processFormCallback($form)
-	{
-		if ($form->isSubmitted()->getValidationScope() !== null) {
-			return;
-		}
-
-		// empty hidden toggles
-		if ($this->emptyHiddenToggleControls) {
-			$toggles = $form->getToggles();
-			foreach ($form->getGroups() as $_group) {
-				$label = $_group->getOption('label');
-				if (isset($toggles[$label]) && $toggles[$label] === false) {
-					foreach ($_group->getControls() as $_control) {
-						$_control->setValue(null);
-					}
-				}
+				$this->onAfterMapToEntity($form);
 			}
-		}
+		});
 
-		if ($this->row) {
-			$this->getForm()->mapToEntity();
-
-			$this->onAfterMapToEntity($form);
-		}
-
-		if (method_exists($this, 'processForm')) {
-			if ($this->row) {
-				$this->processForm($form->getEntity());
+		$this->paramResolvers[] = function($type) {
+			if (is_subclass_of($type, Entity::class)) {
+				return $this->entity;
 			}
-			else {
-				$this->processForm($form->getValues());
-			}
-		}
 
-		if ($form->isValid()) {
-			$this->onAfterProcess($form, $form->getValues());
-		}
+			return false;
+		};
+
+
 	}
 
-	public function render()
+	// we need to call initOnAfterMapToForm last
+	// so we will remove initOnAfterMapToForm, add callbak and add initOnAfterMapToForm again
+	public function setOnAfterInitForm(callable $onAfterInitForm): self
 	{
-		$this->template->setFile(__DIR__ . DIRECTORY_SEPARATOR . 'form.latte');
-
-		$customTemplatePath = (
-		(!empty($this->templateFilename))
-			? $this->templateFilename
-			: str_replace('.php', '.latte', $this->getReflection()->getFileName())
-		);
-
-		if (file_exists($customTemplatePath)) {
-			$this->template->customTemplatePath = $customTemplatePath;
-		}
-
-		if ($this->isAjax) {
-			$this->getForm()->getElementPrototype()->class[] = 'ajax';
-		}
-
-		if ($this->presenter->isAjax()) {
-			$this->redrawControl('formArea');
-		}
-
-		$this->template->render();
-	}
-
-	protected function createComponentForm()
-	{
-		return new Form();
-	}
-
-	protected function _()
-	{
-		return call_user_func_array([$this->getForm()->getTranslator(), 'translate'], func_get_args());
-	}
-
-	public function setRow($row): self
-	{
-		$this->row = $row;
-		return $this;
-	}
-
-	public function setOnBeforeInit(callable $onBeforeInit): self
-	{
-		$this->onBeforeInit[] = $onBeforeInit;
-		return $this;
-	}
-
-	public function setOnAfterInit(callable $onAfterInit): self
-	{
-		$this->onAfterInit[] = $onAfterInit;
+		array_pop($this->onAfterInitForm);
+		$this->onAfterInitForm[] = $onAfterInitForm;
+		$this->onAfterInitForm[] = [$this, 'initOnAfterMapToForm'];
 		return $this;
 	}
 
@@ -208,21 +77,42 @@ abstract class BaseForm extends Control
 		return $this;
 	}
 
-	public function setOnAfterProcess(callable $onAfterProcess): self
+	final public function setEntity(Entity $entity): self
 	{
-		$this->onAfterProcess[] = $onAfterProcess;
+		$this->entity = $entity;
+		if (isset($this->components['form'])) {
+			$this->getForm()->setEntity($entity);
+		}
+		
+		if (method_exists($this, 'initEntity') && !$entity->getId()) {
+			call_user_func([$this, 'initEntity'], $entity);
+		}
+		
 		return $this;
 	}
 
-	/** @deprecated Use setOnAfterProcess instead */
-	public function setOnSuccess(callable $onSuccess): self
+	protected function createComponentForm()
 	{
-		$this->setOnAfterProcess($onSuccess);
-		return $this;
+		return new Form();
 	}
 
-	public function getForm(): Form
+	/**
+	 * @return Form
+	 */
+	public function getForm()
 	{
 		return $this['form'];
+	}
+
+	/**
+	 * @internal
+	 */
+	public function initOnAfterMapToForm(Form $form)
+	{
+		if ($this->getForm()->getEntity()) {
+			$form->mapToForm();
+
+			$this->onAfterMapToForm($form);
+		}
 	}
 }
